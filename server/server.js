bba33c8f9183e69a0f86f9fa6b63b8421018a83b
Emircanbcnk <canemircan973@gmail.com>
2025-11-23 21:14:50 +0300

    Firebase Google Auth hatası

--- server/server.js
+++ server/server.js
@@ -134,12 +134,12 @@ app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com https://apis.google.com; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
     "font-src 'self' https://fonts.gstatic.com; " +
     "img-src 'self' data: https: blob:; " +
-    "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.herokuapp.com wss://*.herokuapp.com ws://*.herokuapp.com http://localhost:3000 wss://localhost:3000 ws://localhost:3000 https://api.binance.com https://api.kucoin.com; " +
-    "frame-src 'none'; " +
+    "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.herokuapp.com wss://*.herokuapp.com ws://*.herokuapp.com http://localhost:3000 wss://localhost:3000 ws://localhost:3000 https://api.binance.com https://api.kucoin.com https://apis.google.com; " +
+    "frame-src 'self' https://*.googleapis.com https://*.gstatic.com https://apis.google.com; " +
     "object-src 'none'; " +
     "base-uri 'self'; " +
     "form-action 'self';"
